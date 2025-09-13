c6f66999a34ebd3b888d42b3ad9ea5d4b50b7f20
PierreLgol <plgol.perso@gmail.com>
2025-09-13 12:22:49 +0200

    refactored the build system, added a check step, and deleted part of the build system related to the static library/root module

--- build.zig
+++ build.zig
@@ -2,39 +2,18 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
-
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe_mod.addImport("taskmaster_lib", lib_mod);
-
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "taskmaster",
-        .root_module = lib_mod,
-    });
-
-    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "taskmaster",
-        .root_module = exe_mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
-
     exe.linkLibC();
     exe.linkSystemLibrary("readline");
-
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -48,19 +27,15 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_module = exe.root_module,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const check_step = b.step("check", "zls helper");
+    check_step.dependOn(&exe.step);
 }
