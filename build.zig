9bdd68ea516e1c436656d67c7d2365a5468fa7bd
PierreLgol <plgol.perso@gmail.com>
2025-09-14 10:44:33 +0200

    working on refactor

--- build.zig
+++ build.zig
@@ -4,22 +4,30 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const anyline_dep = b.dependency("anyline", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const anyline = anyline_dep.module("anyline");
+
     const exe = b.addExecutable(.{
         .name = "taskmaster",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .imports = &.{
+                .{ .name = "anyline", .module = anyline },
+            },
         }),
     });
-    exe.linkLibC();
-    exe.linkSystemLibrary("readline");
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
 
     run_cmd.step.dependOn(b.getInstallStep());
 
+    run_cmd.addFileArg(b.path("config.json"));
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
---
+++ src/Config.zig
@@ -0,0 +1,82 @@
+pub const Config = @This();
+
+arena: heap.ArenaAllocator,
+jobs: ArrayList(Job),
+
+pub const ParsingError = error{
+    ParseError,
+} || Io.Reader.LimitedAllocError || mem.Allocator.Error || json.ParseError(Job);
+
+pub fn init(gpa: mem.Allocator) Config {
+    return .{
+        .arena = heap.ArenaAllocator.init(gpa),
+        .jobs = ArrayList(Job).empty,
+    };
+}
+
+fn allocator(self: *Config) mem.Allocator {
+    return self.arena.allocator();
+}
+
+pub fn parse(config: *Config, file_reader: *Io.Reader) ![]const Job {
+    const content = try file_reader.allocRemaining(config.allocator(), .unlimited);
+    const parsed = try json.parseFromSliceLeaky([]const Job, config.allocator(), content, .{});
+    try config.jobs.ensureTotalCapacityPrecise(config.allocator(), parsed.len);
+    config.jobs.appendSliceAssumeCapacity(parsed);
+    return config.jobs.items;
+}
+
+pub fn deinit(self: *Config) void {
+    defer self.* = undefined;
+    self.arena.deinit();
+}
+
+pub const Job = struct {
+    cmd: []const u8 = "",
+    numprocs: usize = 0,
+    stdout: []const u8 = "",
+    stderr: []const u8 = "",
+    autostart: bool = false,
+    autorestart: []const u8 = "",
+    exitcodes: []const usize = &.{},
+    starttime: usize = 0,
+    startretries: usize = 0,
+    stoptime: usize = 0,
+    stopsignal: []const u8 = "",
+    workingdir: []const u8 = "",
+    umask: []const u8 = "",
+
+    pub fn format(
+        self: @This(),
+        writer: *std.Io.Writer,
+    ) !void {
+        try writer.print(
+            "Job{{\n cmd={s}\n numprocs={d}\n autostart={}\n autorestart={s}\n stdout={s}\n stderr={s}\n workingdir={s}\n umask={s}\n exitcodes={any}\n starttime={d}\n startretries={d}\n stoptime={d}\n stopsignal={s}\n}}",
+            .{
+                self.cmd,
+                self.numprocs,
+                self.autostart,
+                self.autorestart,
+                self.stdout,
+                self.stderr,
+                self.workingdir,
+                self.umask,
+                self.exitcodes,
+                self.starttime,
+                self.startretries,
+                self.stoptime,
+                self.stopsignal,
+            },
+        );
+    }
+};
+
+const std = @import("std");
+const heap = std.heap;
+const mem = std.mem;
+const StringMap = std.StringArrayHashMapUnmanaged;
+const EnumSet = std.EnumSet;
+const ArrayList = std.ArrayListUnmanaged;
+const json = std.json;
+const Io = std.Io;
+const Signal = @import("process.zig").Signal;
