01108ec8a84e78eca9ad7aee8cb95dcf2568a6ad
PierreLgol <plgol.perso@gmail.com>
2025-09-21 17:37:27 +0200

    ok fixed a small bug in the server state machine, forgot to close the loop lol

--- src/server/Server.zig
+++ src/server/Server.zig
@@ -106,6 +106,7 @@ pub fn start(self: *Server, log_file_path: []const u8, unix_sock_path: []const u
 
         .server_needs_to_send_command => {
             sendCommand(&server_process_manager, cmd, &cmd_payload);
+            continue :state .server_needs_to_wait_for_command;
         },
 
         .server_needs_to_stop => {
